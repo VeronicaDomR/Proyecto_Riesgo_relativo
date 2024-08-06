9a5f7577a1be6a05cede02e7db0fd9d003b65ce0
VeronicaDomR <99091553+VeronicaDomR@users.noreply.github.com>
2024-08-06 17:40:58 -0600

    sql

--- SQL/nulls.sql
+++ SQL/nulls.sql
@@ -1,102 +1,130 @@
-#default and user info
+  # Tabla DEFAULT nulos
 SELECT
-user_id,
-default_flag,
-age,
-sex,
-last_month_salary,
-number_dependents,
-COUNT(*) AS duplicate_count
+  'user_id' AS column_name,
+  COUNT(*) AS null_count
 FROM
-`proyecto3-428922.dataset.default_user_info_union`
-GROUP BY 
-user_id,
-default_flag,
-age,
-sex,
-last_month_salary,
-number_dependents
-HAVING
-  COUNT(*) > 1;
-
-#loans_Detail 
+  `proyecto3-428922.dataset.default`
+WHERE
+  user_id IS NULL
+UNION ALL
 SELECT
-  user_id,
-  more_90_days_overdue,
-  debt_ratio,
-  COUNT(*) AS duplicate_count
+  'default_flag' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.default`
+WHERE
+  default_flag IS NULL; 
+
+  #Tabla loans_Detail nulos
+SELECT
+  'user_id' AS column_name,
+  COUNT(*) AS null_count
 FROM
   `proyecto3-428922.dataset.loans_Detail`
-GROUP BY
-  user_id,
-  more_90_days_overdue,
-  debt_ratio
-HAVING
-  COUNT(*) > 1;
-
-
-  #Duplicates loans outstanding
-
-WITH Duplicates AS (
-  SELECT
-    user_id,
-    COUNT(*) AS duplicado
-  FROM
-    `proyecto3-428922.dataset.loans_outstanding`
-  GROUP BY
-    user_id
-  HAVING
-    COUNT(*) > 1
-)
+WHERE
+  user_id IS NULL
+UNION ALL
 SELECT
-  user_id,
-  duplicado AS numero_de_duplicados_user_id
+  'more_90_days_overdue' AS column_name,
+  COUNT(*) AS null_count
 FROM
-  Duplicates;
-
-  #another one detailed
-  WITH Duplicates AS (
-  SELECT
-    user_id,
-    COUNT(*) AS duplicado
-  FROM
-     `proyecto3-428922.dataset.loans_outstanding`
-  GROUP BY
-    user_id
-  HAVING
-    COUNT(*) > 1
-)
+  `proyecto3-428922.dataset.loans_Detail`
+WHERE
+  more_90_days_overdue IS NULL
+UNION ALL
 SELECT
-  l.user_id,
-  l.loan_id,
-  l.loan_type,
-  d.duplicado AS numero_de_duplicados
+  'using_lines_not_secured_personal_assets' AS column_name,
+  COUNT(*) AS null_count
 FROM
-   `proyecto3-428922.dataset.loans_outstanding` l
-JOIN
-  Duplicates d
-ON
-  l.user_id = d.user_id
-ORDER BY
-  l.user_id, l.loan_id;
- 
- #suma
-WITH Duplicates AS (
-  SELECT
-    user_id,
-    COUNT(*) AS duplicado
-  FROM
-   `proyecto3-428922.dataset.loans_outstanding`
-  GROUP BY
-    user_id
-  HAVING
-    COUNT(*) > 1
-)
+  `proyecto3-428922.dataset.loans_Detail`
+WHERE
+  using_lines_not_secured_personal_assets IS NULL
+UNION ALL
 SELECT
-  user_id,
-  duplicado,
-  (SELECT SUM(duplicado) FROM Duplicates) AS total_duplicados
+  'number_times_delayed_payment_loan_30_59_days' AS column_name,
+  COUNT(*) AS null_count
 FROM
-  Duplicates
-ORDER BY
-  user_id;
+  `proyecto3-428922.dataset.loans_Detail`
+WHERE
+  number_times_delayed_payment_loan_30_59_days IS NULL
+UNION ALL
+SELECT
+  'debt_ratio' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.loans_Detail`
+WHERE
+  debt_ratio IS NULL
+UNION ALL
+SELECT
+  'number_times_delayed_payment_loan_60_89_days' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.loans_Detail`
+WHERE
+  number_times_delayed_payment_loan_60_89_days IS NULL;
+  #Tabla loans_outstandig nulos
+SELECT
+  'user_id' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.loans_outstanding`
+WHERE
+  user_id IS NULL
+UNION ALL
+SELECT
+  'loan_id' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.loans_outstanding`
+WHERE
+  loan_id IS NULL
+UNION ALL
+SELECT
+  'loan_type' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.loans_outstanding`
+WHERE
+  loan_type IS NULL; 
+  
+  # Tabla user_info nulos
+SELECT
+  'user_id' AS colum_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.user_info`
+WHERE
+  user_id IS NULL
+UNION ALL
+SELECT
+  'age' AS column_name,
+  COUNT (*) AS null_count
+FROM
+  `proyecto3-428922.dataset.user_info`
+WHERE
+  age IS NULL
+UNION ALL
+SELECT
+  'sex' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.user_info`
+WHERE
+  sex IS NULL
+UNION ALL
+SELECT
+  'last_month_salary' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.user_info`
+WHERE
+  last_month_salary IS NULL
+UNION ALL
+SELECT
+  'number_dependents' AS column_name,
+  COUNT(*) AS null_count
+FROM
+  `proyecto3-428922.dataset.user_info`
+WHERE
+  number_dependents IS NULL;
